e005766fb90863fc4c7b4cb01a0e94a7ce658452
kyori19 <kyori.dis@gmail.com>
2018-12-24 14:19:26 +0900

    Add test for LoginActivity

--- app/src/test/java/net/accelf/mistorb/LoginActivityTest.java
+++ app/src/test/java/net/accelf/mistorb/LoginActivityTest.java
@@ -1,12 +1,17 @@
 package net.accelf.mistorb;
 
+import android.content.Intent;
 import android.net.Uri;
+import android.webkit.WebView;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
 import java.lang.reflect.Method;
+
+import androidx.test.core.app.ApplicationProvider;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -14,6 +19,17 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(RobolectricTestRunner.class)
 public class LoginActivityTest {
+
+    @Test
+    public void test_onCreate() {
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), LoginActivity.class);
+        intent.putExtra(LoginActivity.EXTRA_INSTANCE_DOMAIN, "example.com");
+        LoginActivity activity = Robolectric.buildActivity(LoginActivity.class, intent)
+                .create().get();
+
+        WebView webView = activity.findViewById(R.id.activity_login_web_view);
+        assertEquals("https://example.com/auth/sign_in", webView.getUrl());
+    }
 
     @Test
     public void test_buildLoginUrl() throws Exception {

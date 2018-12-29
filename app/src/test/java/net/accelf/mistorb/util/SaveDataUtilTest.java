ebb6b82ef56fed01703c070088c4780df533bbe2
kyori19 <kyori.dis@gmail.com>
2018-12-29 16:39:41 +0900

    Add test for SaveDataUtil.cookie

---
+++ app/src/test/java/net/accelf/mistorb/util/SaveDataUtilTest.java
@@ -0,0 +1,25 @@
+package net.accelf.mistorb.util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+@RunWith(RobolectricTestRunner.class)
+public class SaveDataUtilTest {
+
+    @Test
+    public void test_cookie() {
+        SaveDataUtil saveData = new SaveDataUtil(ApplicationProvider.getApplicationContext());
+
+        assertNull(saveData.cookie("unknown.domain"));
+
+        saveData.saveCookie("example.com", "test-data");
+        assertEquals("test-data", saveData.cookie("example.com"));
+    }
+
+}

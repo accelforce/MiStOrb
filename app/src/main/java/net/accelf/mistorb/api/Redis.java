caa24a066f6cd5d4d1d3ebf24546ff3abc75ed70
kyori19 <kyori.dis@gmail.com>
2018-12-30 21:33:59 +0900

    Make Redis public

--- app/src/main/java/net/accelf/mistorb/api/Redis.java
+++ app/src/main/java/net/accelf/mistorb/api/Redis.java
@@ -5,13 +5,13 @@ import com.google.gson.annotations.SerializedName;
 public class Redis {
 
     @SerializedName("redis_version")
-    private String redisVersion;
+    public String redisVersion;
     @SerializedName("uptime_in_days")
-    private String uptimeInDays;
+    public String uptimeInDays;
     @SerializedName("connected_clients")
-    private String connectedClients;
+    public String connectedClients;
     @SerializedName("used_memory_human")
-    private String usedMemoryHuman;
+    public String usedMemoryHuman;
     @SerializedName("used_memory_peak_human")
-    private String usedMemoryPeakHuman;
+    public String usedMemoryPeakHuman;
 }
